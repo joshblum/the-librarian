c8beb415a5c7ab232fafaf4ad58f1c75eead1eee
linux-laptop <linux-laptop@linuxlaptop-Dell-System-Inspiron-N4110.(none)>
2013-12-03 13:15:41 -0500

    can split movies now

--- librarian/identifiers/movies/audio/extract-audio.py
+++ librarian/identifiers/movies/audio/extract-audio.py
@@ -3,8 +3,9 @@ import shlex
 import os
 import time
 import json
+import sys
 
-def gen_audio(input_name, output_name):
+def extract_audio(input_name, output_name):
     cmd = "ffmpeg -i %(input_name)s -ab %(bitrate)s -ac %(channels)s -ar %(sampling_frequency)s -vn %(output_name)s" % {
         'input_name': input_name,
         'bitrate': '160k',
@@ -15,8 +16,9 @@ def gen_audio(input_name, output_name):
     return subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT)
 
 def generate_data(input_name):
-    cmd = "./echoprint-codegen %(filepath)s" % {
-        "filepath": "/home/linux-laptop/Desktop/audioextract/tmp/song.mp3"
+    cmd = "./echoprint-codegen %(filepath)s%(filename)s" % {
+        "filepath": "/home/linux-laptop/Desktop/audioextract/",
+        "filename": input_name
     }
     return subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT)
 
@@ -26,14 +28,48 @@ def get_json(s):
 
 def format_text(s):
     o = s.strip('\n')
-    o = o[1:]
-    o = o[:-1]
+    o = o[1:len(o)-1]
     return o
 
-if __name__ == "__main__":
-    r = str(time.time())
-    output = "tmp/tmp-output" + r + ".mp3"
-    jsonData = get_json(generate_data("tmp/tmp-output"))
-    md = jsonData["metadata"]
+def get_audio_fingerprint(pathtofile, input_name):
+    print "Getting audio fingerprint.."
+    video_file = pathtofile + input_name
+    #r = str(time.time())
+    print "Generating audio..."
+    audio_output = "tmp/tmp-output" # + r
+    extract_audio(video_file, audio_output + ".mp3")
+    print "Done."
+    print "Renaming file..."
+    os.rename(audio_output + ".mp3", audio_output)
+    print "Done.."
+    print "Generating data..."
+    jsonData = get_json(generate_data(audio_output))
+    print "Done."
     code = jsonData["code"]
-    print code
+    f = open("results/" + input_name, "w")
+    print "Writing code..."
+    f.write(code)
+    print "Done.."
+    print "Cleaning up..."
+    f.close()
+    os.remove(audio_output)
+    print "Done."
+
+#ffmpeg -i korrabackup -vcodec copy -acodec copy -ss 00:00:00 -t 00:00:30 shortkorra.avi
+
+def split_movie(pathtofile, input_name, start_time="00:00:00", end_time="00:00:30"):
+    video_file = pathtofile + input_name
+    cmd = "ffmpeg -i %(video_file)s -vcodec copy -acodec copy -ss %(start)s -t %(end)s %(output_name)s" % {
+        'video_file': video_file,
+        'start': start_time,
+        'end': end_time,
+        'output_name': pathtofile + "splits/short-" + input_name + str(time.time()) + ".avi"
+    }
+    return subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT)
+
+def split_movie_by_increments(pathtofile, input_name, increments):
+    pass
+    
+if __name__ == "__main__":   
+    get_audio_fingerprint("files/","manofsteelsource")
+    #split_movie("files/","korrasource")
