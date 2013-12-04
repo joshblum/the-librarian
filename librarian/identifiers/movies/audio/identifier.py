afa356f40afc31f740ce32c6fb53330c3239b99b
linux-laptop <linux-laptop@linuxlaptop-Dell-System-Inspiron-N4110.(none)>
2013-12-04 17:55:48 -0500

    periodicity

---
+++ librarian/identifiers/movies/audio/identifier.py
@@ -0,0 +1,44 @@
+"""
+    Movie credit identification handler.
+    Create this object with an input file
+"""
+
+from librarian.constants import LOGGING
+from librarian.identifiers.identifiers import MovieIdentifier
+from extract_frames import extract_frames
+from extract_credits import extract_credits
+from find_films import find_films
+
+import os
+import logging.config
+
+logging.config.dictConfig(LOGGING)
+logger = logging.getLogger(__name__)
+
+
+class MovieAudioIdentifier(MovieIdentifier):
+
+    def __init__(self, srcfile, path):
+        super(MovieIdentifier, self).__init__(srcfile, path)
+        
+        self.credits_path = "%s/credits" % self.path
+        if not os.path.exists(self.credits_path):
+            os.mkdir(self.credits_path)
+
+    def get_titles(self):
+        #optimization: stream frames instead of batch
+        extract_frames(self.srcfile, self.path, self.credits_path)
+        logger.debug("Retrieving credit tokens.")
+
+        credit_tokens = extract_credits(self.credits_path)
+        logger.debug(credit_tokens)
+
+        films = find_films(credit_tokens)
+        logger.debug(films)
+        return films
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) > 1:
+        ident = MovieCreditIdentifier(sys.argv[1], "/tmp")
+        print ident.identify()
---
+++ librarian/identifiers/movies/audio/movie_utils.py
@@ -0,0 +1,92 @@
+import subprocess
+import shlex
+import os
+import time
+import json
+import sys
+
+def extract_audio(input_name, output_name):
+    cmd = "ffmpeg -i %(input_name)s -ab %(bitrate)s -ac %(channels)s -ar %(sampling_frequency)s -vn %(output_name)s" % {
+        'input_name': input_name,
+        'bitrate': '160k',
+        'channels': '2',
+        'sampling_frequency': '44100',
+        'output_name': output_name
+    }
+    return subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT)
+
+def generate_data(input_name):
+    cmd = "./echoprint-codegen %(filepath)s%(filename)s" % {
+        "filepath": "/home/linux-laptop/Desktop/audioextract/",
+        "filename": input_name
+    }
+    return subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT)
+
+def get_json(s):
+    pre_json = format_text(s)
+    return json.loads(pre_json)
+
+def format_text(s):
+    o = s.strip('\n')
+    o = o[1:len(o)-1]
+    return o
+
+def get_audio_fingerprint(pathtofile, input_name):
+    print "Getting audio fingerprint.."
+    video_file = pathtofile + input_name
+    print "Generating audio..."
+    audio_output = "tmp/tmp-output"
+    extract_audio(video_file, audio_output + ".mp3")
+    print "Done."
+    print "Renaming file..."
+    os.rename(audio_output + ".mp3", audio_output)
+    print "Done.."
+    print "Generating data..."
+    jsonData = get_json(generate_data(audio_output))
+    print "Done."
+    code = jsonData["code"]
+    f = open("results/" + input_name, "w")
+    print "Writing code..."
+    f.write(code)
+    print "Done.."
+    print "Cleaning up..."
+    f.close()
+    os.remove(audio_output)
+    print "Done."
+
+#ffmpeg -i korrabackup -vcodec copy -acodec copy -ss 00:00:00 -t 00:00:30 shortkorra.avi
+
+def split_movie(pathtofile, input_name, start_time="00:00:00", end_time="00:00:30"):
+    video_file = pathtofile + input_name
+    cmd = "ffmpeg -i %(video_file)s -vcodec copy -acodec copy -ss %(start)s -t %(end)s %(output_name)s" % {
+        'video_file': video_file,
+        'start': start_time,
+        'end': end_time,
+        'output_name': pathtofile + "splits/short-" + input_name + str(time.time()) + ".avi"
+    }
+    return subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT)
+
+def split_movie_by_increments(pathtofile, input_name, increments):
+    video_file = pathtofile + input_name
+
+def add_time(time, time_to_add):
+    hrs, mins, secs = time.split(":")
+    secs, carry = add_and_carry(secs, time_to_add)
+    mins, carry = add_and_carry(mins, carry)
+    hrs = int(hrs) + carry
+    return "%s:%s:%s" % (stringify(hrs), stringify(mins), stringify(secs))
+
+def add_and_carry(value, value_added):
+    val = int(value) + int(value_added)
+    return (val % 60, val / 60)
+
+def stringify(d):
+    if d < 10:
+        return "0%s" % d
+    else:
+        return "%s" % d
+
+    print hrs
+if __name__ == "__main__":   
+    #get_audio_fingerprint("files/","manofsteelsource")
+    #split_movie("files/","korrasource")
